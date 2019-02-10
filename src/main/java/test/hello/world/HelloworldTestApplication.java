671cebe0c15361791a661d10029cfaf51402e59b
Sivaketty <kvsivakumar.mca@gmail.com>
2019-02-10 19:26:24 +0530

    Initial commit

---
+++ src/main/java/test/hello/world/HelloworldTestApplication.java
@@ -0,0 +1,23 @@
+package test.hello.world;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class HelloworldTestApplication {
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Greetings from Java Tutorial Network commit test";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldTestApplication.class, args);
+	}
+
+}
+
